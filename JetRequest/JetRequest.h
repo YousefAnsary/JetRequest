21980ee0880924051ec7caf0a53fe7758f50f4c7
Yousef <yosefali97@hotmail.com>
2020-09-25 07:49:05 +0200

    Initial Commit

---
+++ JetRequest/JetRequest.h
@@ -0,0 +1,19 @@
+//
+//  JetRequest.h
+//  JetRequest
+//
+//  Created by Yousef on 9/25/20.
+//  Copyright © 2020 Yousef. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JetRequest.
+FOUNDATION_EXPORT double JetRequestVersionNumber;
+
+//! Project version string for JetRequest.
+FOUNDATION_EXPORT const unsigned char JetRequestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JetRequest/PublicHeader.h>
+
+
